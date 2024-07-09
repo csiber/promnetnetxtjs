af03618115a6342cecbaa082bc9df14a6ccda796
Csaba Polyak <csiberius@gmail.com>
2024-07-09 12:38:48 +0000

    "Added head section with favicon link and closed head tag in RootLayout component"

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ export const runtime = 'edge';
 export default function RootLayout({ children }) {
   return (
     <html lang="hu">
+      <head>
         <title>PromNET - Polyák Csaba E.V.</title>
         <meta name="description" content="Weboldal fejlesztés gyorsan, olcsón!" />
         <meta property="og:title" content="PromNET - Polyák Csaba E.V." />
@@ -14,6 +15,8 @@ export default function RootLayout({ children }) {
         <meta property="og:image" content="/logo-white.png" />
         <meta property="og:url" content="https://www.promnet.hu" />
         <meta name="twitter:card" content="summary_large_image" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className="bg-[#171717]">
         <Theming>
           {/* <Dashboard /> */}
