26a2c3951876adac4db21113450be79d668ca0de
Csaba Polyak <csiberius@gmail.com>
2025-08-12 07:42:19 +0200

    fix: enable edge runtime for dynamic routes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    // Enable Next.js image optimization
+    unoptimized: false,
+  },
 };
 
 module.exports = nextConfig;
-module.exports = {
-  images: {
-      unoptimized: true
-  }
-}
