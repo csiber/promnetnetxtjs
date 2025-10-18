3f0d4e06ab79b389d776e46ba5be9efcc99dfe6a
Csaba Polyak <csiberius@gmail.com>
2025-10-18 20:51:29 +0200

    Távolítsd el a jobb oldali panelt a vezérlőpult elrendezéséből

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,13 +1,11 @@
-import View from "@/app/ui/dashboard/View/page";
 import Left from "@/app/dashboard/Left/page";
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className="max-w-[78rem] mx-auto">
-      <div className="gap-4 flex md:mt-5 flex-col md:flex-row">
+    <div className="mx-auto max-w-[78rem]">
+      <div className="flex flex-col gap-4 md:mt-5 md:flex-row">
         <Left />
         {children}
-        <View />
       </div>
     </div>
   );
